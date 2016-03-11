d4630182ed1842c00ceceeaae16858344f89f24c
emyann <renaudin.yann@gmail.com>
2016-03-11 02:01:15 -0500

    Revert "Commit MovieMash app for Jekyll hosting"
    This reverts commit f15df96efc0264c506db3115d08b8239c9656664.

--- app/services/movies.service.js
+++ app/services/movies.service.js
@@ -3,10 +3,10 @@
 		.module('movieMash')
 		.factory('movieService',movieService);
 
-		movieService.$inject=['$http','$log','$q'];
+		movieService.$inject=['$http','$log','$q','IMDB_URL'];
 
-		function movieService($http,$log,$q){
-			var api_baseUrl='http://api.themoviedb.org/3';
+		function movieService($http,$log,$q,IMDB_URL){
+			var api_baseUrl=IMDB_URL;
 			var api_key='9f36e8ba089e4e963fe0528fb83014f9';
 
 			var service={
@@ -15,7 +15,9 @@
 			return service;
 
 			function getTopRatedMovies(){
-				var request='/discover/movie?certification=R&api_key='+api_key+'&page=1&language=fr&include_image_language=fr&sort_by=vote_average.desc&vote_count.gte=1000';
+				var request='/discover/movie?certification=R&api_key='+
+                                api_key+
+                                    '&page=1&language=fr&include_image_language=fr&sort_by=vote_average.desc&vote_count.gte=1000';
 				return $http.get(api_baseUrl+request);
 			}
 		}
--- app/services/scores.service.js
+++ app/services/scores.service.js
@@ -3,11 +3,10 @@
 		.module("movieMash")
 		.factory("scoreService",scoreService);
 
-		scoreService.$inject=['localStorageService','$firebase'];
+		scoreService.$inject=['localStorageService','$firebaseArray','firebaseDataService'];
 
-		function scoreService(localStorageService,$firebase){
-			var firebase_url="https://moviemash.firebaseio.com/data";
-			var scoreSetDb = new Firebase(firebase_url);
+		function scoreService(localStorageService,$firebaseArray,firebaseDataService){
+			var scoresSyncArray = $firebaseArray(firebaseDataService.scores);
 			var service={
 				getAllScores:getAllScores,
 				addPointToMovie:addPointToMovie,
@@ -16,41 +15,22 @@
 			return service;
 
 			function getAllScores(){
-
-				 var ref = new Firebase(firebase_url);
-				  var sync = $firebase(ref);
-				  // create a synchronized array for use in our HTML code
-				  return sync.$asArray();
-
-/*				var keys = localStorageService.keys();
-				var scores=[];
-				angular.forEach(keys, function(key, index) {
-				 scores.push(localStorageService.get(key));
-				});
-				return scores;*/
-
+				return scoresSyncArray;
 			}
 
 			function addPointToMovie(movie){
-				//var points = localStorageService.get(movie.id) ? localStorageService.get(movie.id).points : 0;
-				//points ++;
-				//localStorageService.set(movie.id,{movie:movie,points:points});*/
-				 scoreSetDb.child(movie.id).once('value', function(snapshot) {
-			        var exists = (snapshot.val() !== null);
-			        if(exists){
-			        	var points = snapshot.val().points;
-						points++;
-						var movieRef=scoreSetDb.child(movie.id);
-						movieRef.update({points:points});
-			        }else{
-			        	var movieRef=scoreSetDb.child(movie.id);
-			        	movieRef.set({movie:movie,points:1});
-			        }
-			        
-			      });
-				
-				
-			}
+                var movieObj = scoresSyncArray.$getRecord(movie.id);
+                if(movieObj){                    
+                   movieObj.points ++;
+                    scoresSyncArray.$save({movieObj});
+                }else{
+                    scoresSyncArray.$add({
+                        $id:movie.id,
+                        movie:movie,
+                        points:1
+                    });
+                }
+            }
 
 			function removePointToMovie(movie){
 				var points = localStorageService.get(movie.id) ? localStorageService.get(movie.id).points : 0;
